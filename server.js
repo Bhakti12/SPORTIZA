72579d88ab7f38f005d9f1f1f9bdf9de51527c44
Bhakti Sanghani <52093953+Bhakti12@users.noreply.github.com>
2022-07-14 12:21:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 
-const app = express();
+const app = express();//to start a new express application under app variable.
 
-app.use(bodyparser.urlencoded({extended : true}))
+app.use(bodyparser.urlencoded({extended : true}))//for URL-encoded requests. the extended: true precises that the req.body object will contain values of any type instead of just strings.
